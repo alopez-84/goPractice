6ca088a4528e7d86d56f67cffa18fb1d92f76980
albert.lopez1127@gmail.com <albert.lopez1127@gmail.com>
2025-06-18 16:09:19 -0500

    Fully refactored hello.go, included french and it's test cases for hello_test.go

--- hello_test.go
+++ hello_test.go
@@ -4,15 +4,29 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Albert")
+		got := Hello("Albert", "")
 		want := "Hello, Albert"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("testing with an empty string", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Hello world but in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Testing for French", func(t *testing.T) {
+		got := Hello("", "French")
+		want := "Bonjour, World"
 
 		assertCorrectMessage(t, got, want)
 	})
@@ -21,6 +35,6 @@ func TestHello(t *testing.T) {
 func assertCorrectMessage(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
-		t.Errorf("Want %q, got %q", got, want)
+		t.Errorf("Want %q, got %q", want, got)
 	}
 }
