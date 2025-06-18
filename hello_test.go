0cb29cff67b59b02039c52f6eeb8af2decbf1d8d
albert.lopez1127@gmail.com <albert.lopez1127@gmail.com>
2025-06-18 14:52:01 -0500

    updated go.main and refactored tests

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Albert")
-	want := "Hello Albert"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Albert")
+		want := "Hello, Albert"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("testing with an empty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("Want %q, got %q", got, want)
 	}
 }
