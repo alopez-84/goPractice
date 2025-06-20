8318e176a7e6c5d3bdf8172af12dfcd6797e32e6
albert.lopez1127@gmail.com <albert.lopez1127@gmail.com>
2025-06-20 13:37:26 -0500

    Added a tail sum function and tests for said function.

--- arraysAndSlices/sum.go
+++ arraysAndSlices/sum.go
@@ -8,3 +8,27 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(ints ...[]int) []int {
+	allSum := []int{}
+
+	for _, slice := range ints {
+		sum := Sum(slice)
+		allSum = append(allSum, sum)
+	}
+	return allSum
+}
+
+func SumAllTails(ints ...[]int) []int {
+	allTailSum := []int{}
+
+	for _, slice := range ints {
+		if len(slice) == 0 {
+			allTailSum = append(allTailSum, 0)
+		} else {
+			sum := Sum(slice[1:])
+			allTailSum = append(allTailSum, sum)
+		}
+	}
+	return allTailSum
+}
--- arraysAndSlices/sum_test.go
+++ arraysAndSlices/sum_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -14,5 +15,40 @@ func TestSlice(t *testing.T) {
 		if received != expected {
 			t.Errorf("Wanted %d, got %d, given: %v", expected, received, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("compute sume of 2 Slices", func(t *testing.T) {
+
+		received := SumAll([]int{1, 2}, []int{0, 9})
+		expected := []int{3, 9}
+
+		if !reflect.DeepEqual(received, expected) {
+			t.Errorf("Wanted %v, got %v", expected, received)
+		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	check := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Wanted %v, got %v", want, got)
+		}
+	}
+	t.Run("Compute the sum of all tails", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		check(t, got, want)
+	})
+
+	t.Run("Safely run empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		check(t, got, want)
 	})
 }
